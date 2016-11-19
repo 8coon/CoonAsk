b5b1ddb3762eb1daef929feb0e132e3c16eeb8dc
Kaibito <gpuzzletime@icloud.com>
2016-11-19 12:30:01 +0300

    Still working on that

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# =====[ User vars start ]=====
+PROJECT_NAME=CoonAsk
+# =====[ User vars end ]=====
+
+rm /etc/nginx/sites-enabled/default
+ln -s ~/$PROJECT_NAME/config/nginx.conf /etc/nginx/sites-enabled/default
+
+rm /etc/gunicorn.d/wsgi
+ln -s ~/$PROJECT_NAME/config/gunicorn.conf /etc/gunicorn.d/wsgi
+
+service nginx restart
+service gunicorn restart
